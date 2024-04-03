20c0c1ad0b7b3e937c0f1cec40f8cb70ee022aac
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-03 11:55:44 +0530

    Time: 10 ms (20.26%), Space: 9.4 MB (42.78%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> map1;
+        unordered_map<char, int> map2;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (map1[s[i]] != map2[t[i]]) {
+                return false;
+            }
+            map1[s[i]] = i + 1;
+            map2[t[i]] = i + 1;
+        }
+        
+        return true;
+    }
+};
