992a6fdad4f7ee1d31733aa9fc306e3df777c201
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-05 15:38:31 +0530

    Time: 4 ms (96.15%), Space: 44.3 MB (93.89%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,32 @@
+class MinStack {
+    
+    Stack<Integer> st1;
+    Stack<Integer> st2;
+
+    public MinStack() {
+        st1 = new Stack<>();
+        st2 = new Stack<>();
+    }
+    
+    public void push(int val) {
+        st1.push(val);
+        if(st2.empty() || val <= st2.peek()) st2.push(val);
+    }
+    
+    public void pop() {
+        if(!st1.isEmpty()) {
+            if(st1.peek().equals(st2.peek())) st2.pop();
+            st1.pop();
+        }
+    }
+    
+    public int top() {
+        if(!st1.isEmpty()) return st1.peek();
+        return -1; // or throw an exception indicating stack is empty
+    }
+    
+    public int getMin() {
+        if(!st2.isEmpty()) return st2.peek();
+        return -1; // or throw an exception indicating stack is empty
+    }
+}
