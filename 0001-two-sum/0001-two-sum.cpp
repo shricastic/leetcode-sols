27d473f6732de68014c9c62c3c50328cc7724a37
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-11 22:19:55 +0530

    Time: 73 ms (21.74%), Space: 12.7 MB (73.98%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        res.resize(2,-1);
+        
+        for(int i=0 ; i<nums.size() ; i++){
+            for(int j=i+1 ; j<nums.size() ; j++){
+                if(nums[i]+nums[j]==target){
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        
+        
+        return res;
+    }
+};
