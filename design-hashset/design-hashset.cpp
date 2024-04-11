7a733632227807862735fc21d6a8ac86cf63b3c1
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-11 17:40:27 +0530

    Time: 56 ms, Memory: 48.5 MB - LeetHub

--- design-hashset/design-hashset.cpp
+++ design-hashset/design-hashset.cpp
@@ -1,20 +1,19 @@
 class MyHashSet {
 private:
-    std::vector<bool> set;
-
+    vector<bool> set;
 public:
     MyHashSet() {
         set.resize(1000001, false);
     }
-
+    
     void add(int key) {
         set[key] = true;
     }
-
+    
     void remove(int key) {
         set[key] = false;
     }
-
+    
     bool contains(int key) {
         return set[key];
     }
