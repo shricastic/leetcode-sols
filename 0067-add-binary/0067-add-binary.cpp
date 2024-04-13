348085d63d5e97a2d95c6df316ab2b0f44957229
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-13 16:41:20 +0530

    Time: 4 ms (44.39%), Space: 10.8 MB (19.42%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string res;
-        int c=0, ia=a.size()-1, ib = b.size()-1;
+        int c=0, ia=a.size()-1, ib=b.size()-1;
         
         while(ia>=0 || ib>=0){
             int sum = c;
-            if(ia>=0) sum+= a[ia]-'0';
-            if(ib>=0) sum+= b[ib]-'0';
+            
+            if(ia>=0) sum += a[ia]-'0';
+            if(ib>=0) sum += b[ib]-'0';
             
             res = to_string(sum%2)+res;
             c = sum/2;
@@ -16,8 +17,9 @@ public:
             ib--;
         }
         
-        if(c!=0) res = '1'+res;
-        return res;
+        if(c!=0) res='1'+res;
         
+        return res;
     }
+    
 };
