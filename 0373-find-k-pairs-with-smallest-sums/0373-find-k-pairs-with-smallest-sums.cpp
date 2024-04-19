f1f23a6a63226872de5aebc6f11c8654816492ea
Shrikrushna C Gundre <120995762+Shricastic7@users.noreply.github.com>
2024-04-20 01:22:27 +0530

    Time: 233 ms (32.23%), Space: 134.5 MB (83.00%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        int n = nums1.size(), m = nums2.size();
-        vector<vector<int>> ans;
-        for(int i = 0; i < n; i++)
-            pq.push({nums1[i]+nums2[0],{i,0}});
-        while(!pq.empty() && k--)
+         priority_queue<pair<int,pair<int,int>>> pq;
+        for(int i=0;i<nums1.size();i++)
         {
-            pair<int,pair<int,int>> tp = pq.top();
-            int x = tp.second.first, y = tp.second.second;
-            pq.pop();
-            ans.push_back({nums1[x], nums2[y]});
-            if(y != m-1)
-                pq.push({nums1[x]+nums2[y+1],{x, y+1}});
+            for(int j=0;j<nums2.size();j++)
+            {
+                int sum=nums1[i]+nums2[j];
+                if(pq.size()<k)pq.push({sum,{nums1[i],nums2[j]}});
+                else if(sum<pq.top().first)
+                {
+                    pq.pop();
+                    pq.push({sum,{nums1[i],nums2[j]}});
+                } else{
+                    break;
+                }
+            }
         }
+        vector<vector<int>> ans;
+        while(!pq.empty())
+        {
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
