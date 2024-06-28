a57f93033c03a378a8b5a18374857a72d1c9973f
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-06-28 14:35:45 +0530

    Time: 4 ms (84.31%), Space: 8.2 MB (47.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -6,25 +6,15 @@ public:
         while(num>0){
             int ld = num%10;
             num /=10;
-            
-            
             if(res > (INT_MAX/10)+ld) return 0;
-            
             res = (res*10)+ld;
-        }
-        
+        } 
         return res;
     }
     
     bool isPalindrome(int x) {
         if(x<0) return false;
-        
         int tmp = reverse(x);
-        
-        if(x-tmp==0){
-            return true;
-        }
-    
-        return false;
+        return x==tmp;
     }
 };
