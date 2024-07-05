5b5dbfc12dba26dff315ce191dccddd26198a641
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-07-05 15:38:36 +0530

    Time: 60 ms (24.54%), Space: 49.2 MB (54.85%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    
+    nums.forEach(function(val, idx){
+        init = fn(init, val)
+    })
+    
+    return init;
+};
