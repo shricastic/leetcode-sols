8e312805d78283c1086a74c56aa89f9c4341d697
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-07-18 17:38:34 +0530

    Time: 17 ms (11.25%), Space: 59.6 MB (9.96%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -5,7 +5,7 @@ public:
         int carry = 0, i = num1.size()-1, j = num2.size()-1;
         
         while(i>=0 || j>=0 || carry>0){
-            long sum = 0;
+            int sum = 0;
             if(i>=0) sum += (num1[i]-'0'), i--;
             if(j>=0) sum += (num2[j]-'0'), j--;
             
