f552b2ac658d0c9353690cae24f56d9a234b397f
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-07-19 14:52:00 +0530

    Time: 15 ms (23.65%), Space: 14.1 MB (27.15%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int row = board.size(), col = board[0].size();
+        int rows = board.size(), cols = board[0].size();
         queue<pair<int, int>> q;
-
-        for (int r = 0; r < row; ++r) {
-            for (int c = 0; c < col; ++c) {
-                if ((r == 0 || r == row - 1 || c == 0 || c == col - 1) && board[r][c] == 'O') {
-                    q.push({r, c});
+        
+        for(int row = 0; row<rows ; row++){
+            for(int col = 0 ; col<cols ; col++){
+                if((row == 0 || row==rows-1 || col==0 || col==cols-1) && board[row][col]=='O'){
+                    q.push({row, col});
                 }
             }
         }
-
-        while (!q.empty()) {
-            int r = q.front().first, c = q.front().second;
+        
+        while(!q.empty()){
+            auto [r, c] = q.front();
             q.pop();
-            if (r >= 0 && r < row && c >= 0 && c < col && board[r][c] == 'O') {
-                board[r][c] = 'B'; 
-                if (r - 1 >= 0) q.push({r - 1, c});
-                if (r + 1 < row) q.push({r + 1, c});
-                if (c - 1 >= 0) q.push({r, c - 1});
-                if (c + 1 < col) q.push({r, c + 1});
+            
+            if(r<rows and r>=0 and c<cols and c>=0 and board[r][c]=='O'){
+                board[r][c] = 'T';
+                if(r-1 >= 0) q.push({r-1, c});
+                if(r+1 < rows) q.push({r+1, c});
+                if(c-1 >= 0) q.push({r, c-1});
+                if(c+1 < cols) q.push({r, c+1});
             }
         }
-
-        for (int r = 0; r < row; ++r) {
-            for (int c = 0; c < col; ++c) {
-                if (board[r][c] == 'O') {
-                    board[r][c] = 'X';
-                } else if (board[r][c] == 'B') {
-                    board[r][c] = 'O';
-                }
+        
+        for(int row = 0; row<rows ; row++){
+            for(int col = 0 ; col<cols ; col++){
+                if(board[row][col]=='O') board[row][col] = 'X';
+                else if(board[row][col] == 'T') board[row][col] = 'O';
             }
-        }
+        }       
     }
 };
