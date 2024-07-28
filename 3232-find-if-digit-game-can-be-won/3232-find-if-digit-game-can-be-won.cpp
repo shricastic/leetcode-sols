931ea069125bc3eddc4a6ba844c56ef19c3ac338
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-07-28 23:58:29 +0530

    Time: 11 ms (16.67%), Space: 28.2 MB (16.67%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -3,9 +3,7 @@ public:
     bool canAliceWin(vector<int>& nums) {
         int sum=0;
         
-        for(int i : nums){
-            i<10? sum+=i : sum-=i;
-        }
+        for(int i : nums) i<10? sum+=i : sum-=i;
         
         return sum==0 ? false : true;        
     }
