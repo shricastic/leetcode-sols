0b76a525aa73a704af6abe1a10b1e95e5c64b77e
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-02 15:05:19 +0530

    Time: 75 ms (29.27%), Space: 93.6 MB (6.88%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int totalOnes = count(nums.begin(), nums.end(), 1);
+        int totalOnes = count(nums.begin(), nums.end(), 1), curOnes = 0, maxOnes = 0;
         vector<int> tmp(nums);
         tmp.insert(tmp.end(), nums.begin(), nums.end());
-        int curOnes = 0;
         
-        for(int i=0 ; i<totalOnes ; i++){
-            if(tmp[i] == 1) curOnes++;
-        }
+        for(int i=0 ; i<totalOnes ; i++) if(tmp[i] == 1) curOnes++;
         
-        int maxOnes = curOnes;
+        maxOnes = curOnes;
         
         for(int i=totalOnes ; i<tmp.size() ; i++){
             if(tmp[i] == 1) curOnes++;
-            if(tmp[i-totalOnes] == 1) curOnes--;
-            
+            if(tmp[i-totalOnes] == 1) curOnes--;   
             maxOnes = max(curOnes, maxOnes);
         }
         
-        return totalOnes-maxOnes;
+        return totalOnes - maxOnes;
     }
 };
