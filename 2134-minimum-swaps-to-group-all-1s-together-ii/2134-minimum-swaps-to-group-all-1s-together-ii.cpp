5535c6df0382aa9d01ddb7486f83d157dd65f73c
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-02 15:09:54 +0530

    Time: 83 ms (11.00%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int totalOnes = count(nums.begin(), nums.end(), 1), curOnes = 0, maxOnes = 0;
-        vector<int> tmp(nums);
-        tmp.insert(tmp.end(), nums.begin(), nums.end());
+        int totalOnes = count(nums.begin(), nums.end(), 1), 
+        n = nums.size(), curOnes = 0, maxOnes = 0;
         
-        for(int i=0 ; i<totalOnes ; i++) if(tmp[i] == 1) curOnes++;
+        for(int i=0 ; i<totalOnes ; i++) if(nums[i] == 1) curOnes++;
         
         maxOnes = curOnes;
         
-        for(int i=totalOnes ; i<tmp.size() ; i++){
-            if(tmp[i] == 1) curOnes++;
-            if(tmp[i-totalOnes] == 1) curOnes--;   
+        for(int i=totalOnes ; i<n*2 ; i++){
+            if(nums[i%n] == 1) curOnes++;
+            if(nums[(i-totalOnes) % n] == 1) curOnes--;   
             maxOnes = max(curOnes, maxOnes);
         }
         
