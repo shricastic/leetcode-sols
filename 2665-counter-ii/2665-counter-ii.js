b2c322b18443468778f54321db169f27c4de5b3c
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-04 12:03:21 +0530

    Time: 51 ms (90.76%), Space: 51 MB (94.13%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -3,18 +3,12 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    let val = init
-    return{
-        increment: function(){
-            return ++val
-        },
-        decrement: function(){
-            return --val
-        },
-        reset :function(){
-            val = init 
-            return val
-        }
+    let x = init
+    
+    return {
+        increment: function(){return ++x},
+        reset: function(){x = init; return x},
+        decrement: function(){return --x}
     }
 };
 
