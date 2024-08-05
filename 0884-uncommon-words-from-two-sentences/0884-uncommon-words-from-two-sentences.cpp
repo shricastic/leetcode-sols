ff8cec132e211df8aaf84cc5670fb201e58c71c1
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-05 13:22:18 +0530

    Time: 4 ms (42.38%), Space: 8.8 MB (41.94%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -1,24 +1,23 @@
 class Solution {
 private: 
-    vector<string> split(string s, char delimiter){
+    void splitNmap(string s, char delimiter, unordered_map<string, int>& map){
         vector<string> tokens;
         string token = "";
         
         for(char c : s){
-            if(c == delimiter){if(!token.empty()) tokens.push_back(token), token.clear();}
+            if(c == delimiter){if(!token.empty()) map[token]++, token.clear();}
             else {token+=c;}
         }
         
-        if(!token.empty()) tokens.push_back(token);
-        return tokens;
+        if(!token.empty()) map[token]++;
     }
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         vector<string> res;
         unordered_map<string, int> map;
         
-        for(string s : split(s1, ' ')) map[s]++;
-        for(string s : split(s2, ' ')) map[s]++;
+        splitNmap(s1, ' ', map);
+        splitNmap(s2, ' ', map);
         
         for(auto it : map) if(it.second == 1) res.push_back(it.first);
         
