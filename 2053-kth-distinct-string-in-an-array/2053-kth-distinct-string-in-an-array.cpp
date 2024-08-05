12963ce8a9524cde6b05ff48ded745fcdf112ede
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-05 11:29:29 +0530

    Time: 8 ms (96.95%), Space: 18.5 MB (20.16%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -3,21 +3,9 @@ public:
     string kthDistinct(vector<string>& arr, int k) {
         unordered_map<string, int> map;
         
-        for(string s : arr){
-            if(map.count(s)>0){
-                map[s]++;
-            } else{
-                map[s] = 1;
-            }
-        }
-        
-        for(string s : arr){
-            if(map[s] == 1){
-                if(k==1) return s;
-                k--;
-            }
-        }
-        
+        for(string s : arr) map.count(s)>0 ? map[s]++ : map[s] = 1;       
+        for(string s : arr) if(map[s] == 1) { if(k--==1) return s; }
+    
         return "";
     }
 };
