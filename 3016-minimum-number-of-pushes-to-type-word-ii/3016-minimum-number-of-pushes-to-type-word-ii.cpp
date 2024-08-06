872e50e6a63ff4da6084a1b0320b6d0ae1786ea9
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-06 12:50:26 +0530

    Time: 74 ms (55.49%), Space: 27.7 MB (17.31%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,16 +5,9 @@ public:
         int res = 0, cnt=0;
         
         for(char c : word) cmap[c-97]++;
-        sort(cmap.rbegin(), cmap.rend());
-        
-        for(int i : cmap){
-            if(i>0){
-                cout<< i << " ";
-                res += i* ((cnt/8)+1);
-                cnt++;
-            }
-        }
-        
+        sort(cmap.rbegin(), cmap.rend());        
+        for(int i : cmap) if(i>0) res += i* ((cnt++/8)+1);
+
         return res;
     }
 };
