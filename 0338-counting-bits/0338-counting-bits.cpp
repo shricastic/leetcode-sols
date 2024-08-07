8ca79c3484daacc8d6d68d5d119d1a1841c398b0
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-07 09:09:29 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (31.20%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,12 +1,10 @@
 class Solution {
 private:
     int count(int n, vector<int>& dp){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+        if(n <= 1) return n;
         
         if(dp[n] != -1) return dp[n];
-        dp[n] = count(n/2, dp) + n%2;
-        return dp[n];
+        return dp[n] = count(n/2, dp) + n%2;
     }
 public:
     vector<int> countBits(int n) {
