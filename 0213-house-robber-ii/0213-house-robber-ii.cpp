20d8d75101882f889fa21299fca7caf9601508ea
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-15 21:09:01 +0530

    Time: 4 ms (32.62%), Space: 9.7 MB (33.77%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int steal(vector<int>& nums, vector<int>& dp, int start, int end){
+        if(start > end) return 0;
+        if(dp[start] != -1) return dp[start];
+        
+        int take = nums[start] + steal(nums, dp, start+2, end);
+        int skip = 0 + steal(nums, dp, start+1, end);
+        
+        dp[start] = max(take, skip);
+        
+        return dp[start];
+    }
+public:
+    int rob(vector<int>& nums) {
+        //house robber II
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0], nums[1]);
+        
+        
+        vector<int> dp1(nums.size(), -1), dp2(nums.size(), -1);
+        return max(steal(nums, dp1, 1, nums.size()-1), steal(nums, dp2, 0, nums.size()-2));
+    }
+};
