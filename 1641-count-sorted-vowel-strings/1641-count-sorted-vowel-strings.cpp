a5ca02f55c8c2e29399f2f7916610352a5d41e43
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-08-31 10:54:39 +0530

    Time: 87 ms (8.91%), Space: 7.4 MB (54.48%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    
+    void lexiString(int n, int index, int len, int& cnt) {
+        if (len == n) {
+            cnt++;
+            return;
+        }
+        
+        for (int i = index; i < vowels.size(); i++) {
+            lexiString(n, i, len + 1, cnt);
+        }
+    }
+
+public:
+    int countVowelStrings(int n) {
+        int cnt = 0;
+        lexiString(n, 0, 0, cnt);
+        return cnt;
+    }
+};
