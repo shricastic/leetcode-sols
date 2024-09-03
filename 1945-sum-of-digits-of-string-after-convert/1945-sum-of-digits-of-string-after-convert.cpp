2891629f89b288be5f2370901128868d369a9744
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-09-03 13:27:30 +0530

    Time: 3 ms (51.28%), Space: 10.1 MB (21.06%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int res = 0;
+        string str = "";
+        
+        for(char c : s) str += to_string(c-'`');
+        
+        while(k>0){
+            res = 0;
+            for(char c : str) res+= c-'0';
+            str = to_string(res);
+            k--;
+        }
+        
+        return res;        
+    }
+};
