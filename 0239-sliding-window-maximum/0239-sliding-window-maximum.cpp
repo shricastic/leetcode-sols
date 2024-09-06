4d9da20cf37a469491fe5182517b97db4969f7f4
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-09-06 22:33:16 +0530

    Time: 211 ms (22.41%), Space: 152.5 MB (20.87%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,23 +4,16 @@ public:
         vector<int> res;
         priority_queue<pair<int, int>> pq;  
         
-        for(int i = 0; i < k; i++){
-            pq.push({nums[i], i});
-        }
-        
+        for(int i = 0; i < k; i++) pq.push({nums[i], i}); 
         res.push_back(pq.top().first);
         
         int fi = 0, li = k;
         
         while(li < nums.size()){
-            while(!pq.empty() && pq.top().second <= fi) {
-                pq.pop();
-            }
-            
-            pq.push({nums[li], li});
-            
+            while(!pq.empty() && pq.top().second <= fi) pq.pop();
+           
+            pq.push({nums[li], li});    
             res.push_back(pq.top().first);
-            
             fi++, li++;
         }
         
