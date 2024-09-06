e05eda09dd2100d47bb8dc9ca0086664ce189727
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-09-06 21:18:29 +0530

    Time: 27 ms (7.65%), Space: 19.3 MB (6.97%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
-public:
-    vector<vector<int> > combinationSum2(vector<int> &num, int target) 
-    {
-        vector<vector<int>> res;
-        sort(num.begin(),num.end());
-        vector<int> local;
-        findCombination(res, 0, target, local, num);
-        return res;
-    }
-    void findCombination(vector<vector<int>>& res, const int order, const int target, vector<int>& local, const vector<int>& num)
-    {
-        if(target==0)
-        {
-            res.push_back(local);
+    vector<vector<int>> res;
+    
+    void combination(const vector<int>& candidates, vector<int> cur, int target, int sum, int i){
+        if(sum == target){
+            res.push_back(cur);
             return;
         }
-        else
-        {
-            for(int i = order;i<num.size();i++) 
-            {
-                if(num[i]>target) return;
-                if(i&&num[i]==num[i-1]&&i>order) continue;
-                local.push_back(num[i]),
-                findCombination(res,i+1,target-num[i],local,num); 
-                local.pop_back();
-            }
+        
+        if(sum > target || i >= candidates.size()) return;
+        
+        for(int j = i; j < candidates.size(); ++j){
+            if(j > i && candidates[j] == candidates[j-1]) continue; 
+            cur.push_back(candidates[j]);
+            combination(candidates, cur, target, sum + candidates[j], j + 1);
+            cur.pop_back();
         }
+    }
+    
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        combination(candidates, {}, target, 0, 0);
+        return res;
     }
 };
