1946ec9137d38aef22f92573d65a460d6ec9b3b5
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-09-12 08:48:19 +0530

    Time: 51 ms (38.32%), Space: 33.9 MB (87.14%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    bool isConsistent(unordered_set<string> &set, string s){
-        for(char c : s) if(set.count(to_string(c)) == 0) return false;
+    bool isConsistent(unordered_set<char> &set, string s){
+        for(char c : s) if(set.count(c) == 0) return false;
         return true;
     }
     
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        unordered_set<string> set;
-        for(char c : allowed) set.insert(to_string(c));
+        unordered_set<char> set;
+        for(char c : allowed) set.insert(c);
         
         int cnt = 0;
         for(string s : words) if(isConsistent(set, s)) cnt++;
