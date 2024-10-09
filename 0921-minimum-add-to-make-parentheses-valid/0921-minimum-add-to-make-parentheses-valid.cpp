994e67a1c01c0431e3afd1b71cb029fe3bb10ea4
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-10-10 03:34:52 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (94.08%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int open = 0, close = 0;
-        
-        for(char c : s){
-            c=='(' ? open++ : open>0 ? open-- : close++;
-        }
-        
+        for(char c : s) c=='(' ? open++ : open>0 ? open-- : close++;
         return open+close;
     }
 };
