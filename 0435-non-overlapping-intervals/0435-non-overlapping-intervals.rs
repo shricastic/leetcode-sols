275b0445d77d7dc1d552e0584dee861b50427824
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-11-09 15:55:30 +0530

    Time: 45 ms (50.00%), Space: 10.2 MB (50.94%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.rs
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.rs
@@ -1,16 +1,17 @@
 impl Solution {
     pub fn erase_overlap_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
-        intervals.sort_by_key(|x| x[1]);
+        intervals.sort();
 
         let mut res = 0;
         let mut prev_end = intervals[0][1];
 
         for interval in intervals.iter().skip(1) {
             let (start, end) = (interval[0], interval[1]);
-            if start >= prev_end {
-                prev_end = end;
-            } else {
+            if start < prev_end {
                 res += 1;
+                prev_end = prev_end.min(end);
+            } else {
+                prev_end = end;
             }
         }
 
