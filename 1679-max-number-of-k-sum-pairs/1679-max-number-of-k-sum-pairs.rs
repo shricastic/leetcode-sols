36f1aa89936964023821c17e6b3cd0b35f15e179
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-11-17 01:46:42 +0530

    Time: 23 ms (65.63%), Space: 3.5 MB (28.48%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut map = HashMap::new();
+        let mut res = 0;
+        
+        for &i in &nums {
+            if let Some(cnt) = map.get_mut(&(k-i)) {
+                if *cnt>0 { res+=1; *cnt-=1; }
+                else { *map.entry(i).or_insert(0) += 1 }
+            } else {
+                *map.entry(i).or_insert(0) += 1;
+            }
+        }
+        
+        res
+    }
+}
