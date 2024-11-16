5e869fcc3d7e4c3b6ec780d5847c88b3b4753ec8
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-11-17 01:08:09 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (87.50%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.rs
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.rs
@@ -3,23 +3,18 @@ use std::cmp;
 impl Solution {
     pub fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let length = nums.len();
-        let mut result = vec![-1; length - k as usize + 1]; 
+        let mut res = vec![-1; length - k as usize + 1]; 
 
         for start in 0..=length - k as usize {
-            let mut is_consecutive_and_sorted = true;
+            let mut inc_sorted = true;
 
-            for index in start..start + k as usize - 1 {
-                if nums[index + 1] != nums[index] + 1 {
-                    is_consecutive_and_sorted = false;
-                    break;
-                }
+            for j in start..start + k as usize - 1 {
+                if nums[j+1] != nums[j]+1 { inc_sorted = false; break; }
             }
 
-            if is_consecutive_and_sorted {
-                result[start] = nums[start + k as usize - 1];
-            }
+            if inc_sorted { res[start] = nums[start + k as usize - 1]; }
         }
 
-        result
+        res
     }
 }
