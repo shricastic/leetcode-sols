06e69ad47a41f25f4f08598110cf312141ffc372
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-11-19 22:55:04 +0530

    Time: 0 ms (100.00%), Space: 3.6 MB (100.00%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.rs
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.rs
@@ -1,12 +1,19 @@
 impl Solution {
     pub fn maximum_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
         let (mut f, mut res, mut sum, mut j) = ([0; 100_001], 0, 0, 0);
+        
         for (i, &n) in nums.iter().enumerate() {
             while i - j + 1 > k as usize || f[n as usize] > 0 {
-                sum -= nums[j] as i64; f[nums[j] as usize] -= 1; j += 1
+                sum -= nums[j] as i64; 
+                f[nums[j] as usize] -= 1; j += 1
             }
-            sum += n as i64; f[n as usize] += 1;
+            
+            sum += n as i64; 
+            f[n as usize] += 1;
+            
             if i - j + 1 == k as usize { res = res.max(sum) }
-        }; res
+        }; 
+        
+        res
     }
 }
