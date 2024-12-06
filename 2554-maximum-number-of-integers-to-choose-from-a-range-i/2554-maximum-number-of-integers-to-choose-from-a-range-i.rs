85d97c45fa3f032f51fb9a0de342fd8ae23a38e9
Shrikrushna C Gundre <120995762+shricastic@users.noreply.github.com>
2024-12-07 04:16:09 +0530

    Time: 32 ms (25.00%), Space: 2.6 MB (75.00%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.rs
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.rs
@@ -5,14 +5,11 @@ impl Solution {
         let mut set = HashSet::new();
         let (mut sum, mut cnt) = (0, 0);
         
-        
-        for num in &banned{
-            set.insert(num);
-        }
+        for num in &banned{ set.insert(num); }
         
         for i in 1..=n{
-            if set.contains(&(i as i32)) { continue; } 
             if (sum + i as i32) > max_sum { break; }
+            if set.contains(&(i as i32)) { continue; } 
             sum+=i as i32;
             cnt+=1;
         }
