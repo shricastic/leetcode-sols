9e1998151654e523c3ee8769d11087a6ac55b92a
Shri ;) <120995762+shricastic@users.noreply.github.com>
2025-06-21 23:42:50 +0530

    0046-PERMUTATIONS Stats: Time: 0 ms (100%), Space: 12.9 MB (8.08%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,28 @@
 class Solution {
 private:
     vector<vector<int>> res;
-    void permute(vector<int>& nums, vector<int> cur, int i){
+    void permute(vector<int>& nums, vector<int> cur, vector<bool> used, int i){
         if(cur.size() == nums.size()){
             res.push_back(cur);
             return;
         }
 
         for(int i=0 ; i<nums.size() ; i++){
-            if(find(cur.begin(), cur.end(), nums[i]) != cur.end()) continue;
+            if(used[i]) continue;
+            
+            used[i] = true;
             cur.push_back(nums[i]);
-            permute(nums, cur, i);
+            
+            permute(nums, cur, used, i);
+
+            used[i] = false;
             cur.pop_back();
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        permute(nums, {}, 0);
+        vector<bool> used(nums.size(), false);
+        permute(nums, {}, used, 0);
         return res;
     }
 };
