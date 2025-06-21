29951792d0e688783fe64ad11889096cd910fcd0
Shri ;) <120995762+shricastic@users.noreply.github.com>
2025-06-22 00:17:18 +0530

    0078-SUBSETS Stats: Time: 3 ms (32.69%), Space: 12.8 MB (27.89%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     vector<vector<int>> res;
     vector<int> nums;
-    void solve(vector<int> cur, int i){
+    void solve(vector<int> &cur, int i){
         if(i == nums.size()){
             res.push_back(cur);
             return;
@@ -11,11 +11,13 @@ private:
         solve(cur, i+1);
         cur.push_back(nums[i]);
         solve(cur, i+1);
+        cur.pop_back();
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         this->nums = nums;
-        solve({}, 0);
+        vector<int> cur;
+        solve(cur, 0);
         return res; 
     }
 };
