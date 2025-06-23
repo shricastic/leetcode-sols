ef1773b95aba2d4e61e8475e194b4fd1891d70c4
Shri ;) <120995762+shricastic@users.noreply.github.com>
2025-06-23 17:21:02 +0530

    Time: 5 ms (44.93%), Space: 19 MB (20.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,41 @@
 class Solution {
 private:
-    vector<int> vis;
-    stack<int> st;
-    bool dfs(int node, vector<vector<int>>& adjList){
-        if(vis[node] == 1) return true;
-        if(vis[node] == 2) return false;
+    vector<int> vis, res;
+    bool cycle = false;
+    void dfs(int node, vector<vector<int>>& adjList){
+        if(cycle || vis[node]!=0) return;
 
         vis[node] = 1;
-       
-        for(auto n: adjList[node]){
-            if(dfs(n, adjList)) return true;
+
+        for(int nbr: adjList[node]){
+            if(vis[nbr] == 1){
+                cycle = true;
+                return;
+            }
+            dfs(nbr, adjList);
         }
 
         vis[node] = 2;
 
-        st.push(node);
-        return false;
+        res.push_back(node);
     }
+
+
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjList(n);
-        for(auto& p : prerequisites){
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+        vis.resize(N, 0); 
+        vector<vector<int>> adjList(N);
+
+        for(auto p: prerequisites){
             adjList[p[1]].push_back(p[0]);
         }
 
-        vis.resize(n, 0);
-        for(int i=0 ; i<n ; i++){
-            if(vis[i] == 0){
-                if(dfs(i, adjList)) return {};
-            }
+        for(int i=0 ; i<N ; i++){
+            if(vis[i] == 0) dfs(i, adjList);
+            if(cycle) return {};
         }
-        
-        vector<int> res;
-        while(!st.empty()){
-            res.push_back(st.top()); st.pop();
-        }
+
+        reverse(res.begin(), res.end());
 
         return res;
     }
